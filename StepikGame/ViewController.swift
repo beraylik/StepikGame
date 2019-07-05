405167fccba97c141805fce86c401d89c25ff610
Берайлик Генрих Рахматжонович <berajlik.g@milandr.ru>
2019-07-05 17:04:00 +0300

    Initial Commit

---
+++ StepikGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepikGame
+//
+//  Created by Миландр on 05/07/2019.
+//  Copyright © 2019 beraylik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
